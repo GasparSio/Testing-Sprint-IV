320462e4c26f959161ad802d7b20eac54c8179ef
Gaspar Sio <sio.gaspar@gmail.com>
2023-10-09 21:09:23 +0200

    testing more examples with jest

--- index.js
+++ index.js
@@ -1,49 +1,101 @@
-class Room{
-  constructor(name, bookings, rate, discount){
+class Room {
+  constructor(name, bookings, rate, discount) {
     this.name = name;
     this.bookings = bookings;
     this.rate = rate;
     this.discount = discount;
   }
-  isOccupied(date){
+  isOccupied(date) {
     const myDate = new Date(date);
 
-    for(let i = 0; i < this.bookings.length; i++){
-      const startDate = new Date(this.bookings[i].checkIn)
-      const endDate = new Date(this.bookings[i].checkOut)
-      
-      if(myDate >= startDate && myDate <= endDate){
+    for (let i = 0; i < this.bookings.length; i++) {
+      const startDate = new Date(this.bookings[i].checkin);
+      const endDate = new Date(this.bookings[i].checkout);
+
+      if (myDate >= startDate && myDate <= endDate) {
         return true;
+      }
     }
-  }
+
     return false;
   }
-  occupancyPercentage(startingDate, endingDate){
-    const startDate = new Date(startingDate)
-    const endDate = new Date(endingDate)
 
-    const totalDaysInrange = (endDate - startDate) / (24 * 60 * 60 * 1000) + 1;
+  occupancyPercentage(startingDate, endingDate) {
+    const startDate = new Date(startingDate);
+    const endDate = new Date(endingDate);
 
+    startDate.setHours(0, 0, 0, 0);
+    endDate.setHours(23, 59, 59, 999);
+
+    let totalDaysInRange = 0;
     let occupiedDays = 0;
 
-    for(let i = startDate; i <= endDate; i.setDate(i.getDate() + 1)){
-      if (this.isOccupied(i)){
+    for (let currentDate = startDate; currentDate <= endDate; currentDate.setDate(currentDate.getDate() + 1)) {
+        
+      totalDaysInRange++;
+
+      let isOccupied = false;
+
+      for (const booking of this.bookings) {
+        const bookingStartDate = new Date(booking.checkin);
+        const bookingEndDate = new Date(booking.checkout);
+        bookingStartDate.setHours(0, 0, 0, 0);
+        bookingEndDate.setHours(23, 59, 59, 999);
+
+        if (currentDate >= bookingStartDate && currentDate <= bookingEndDate) {
+          isOccupied = true;
+          break;
+        }
+      }
+
+      if (isOccupied) {
         occupiedDays++;
       }
     }
-    const percentage = (occupiedDays / totalDaysInrange) * 100;
-    console.log(percentage);
+
+    if (totalDaysInRange === 0) {
+      return 0;
+    }
+
+    const percentage = (occupiedDays / totalDaysInRange) * 100;
+
     return parseFloat(percentage.toFixed(1));
+  }
+
+  static totalOccupancyPercentage(rooms, startDate, endDate) {
     
+    if (!Array.isArray(rooms) || rooms.every((room) => !(room instanceof Room))) {
+        return 0;
+    }
+
+    function countDays(startDate, endDate) {
+      const oneDay = 24 * 60 * 60 * 1000;
+      return Math.round(Math.abs((startDate - endDate) / oneDay)) + 1;
+    }
+
+    let totalOccupiedDays = 0;
+    let totalDaysInRange = countDays(new Date(startDate), new Date(endDate));
+
+    if (totalDaysInRange === 0) {
+      return 0;
+    }
+
+    rooms.forEach((room) => {
+      totalOccupiedDays += room.occupancyPercentage(startDate, endDate);
+    });
+
+    const percentage = (totalOccupiedDays / rooms.length).toFixed(1);
+
+    return parseFloat(percentage);
   }
 }
 
 class Booking{
-  constructor(name, email, checkIn, checkOut, discount, room){
+  constructor(name, email, checkin, checkout, discount, room){
     this.name = name;
     this.email = email;
-    this.checkIn = checkIn;
-    this.checkOut = checkOut;
+    this.checkin = checkin;
+    this.checkout = checkout;
     this.discount = discount;
     this.room = room;
   }
@@ -53,4 +105,3 @@ module.exports = {
   Room,
   Booking
 } 
-
--- index.test.js
+++ index.test.js
@@ -19,7 +19,7 @@ describe('Testing class Room', () => {
   })
   it('isOccupied is false cuando la habitacion no esta ocupada en una fecha dada ', () => {
     const room = {
-      name: 'Habitacion 1',
+      name: 'Habitacion 2',
       rate: 150,
       discount: 10,
     };
@@ -36,7 +36,7 @@ describe('Testing class Room', () => {
   });
   it('isOccupied is boolean sin importar el dato que le pasemos', () => {
     const room = {
-      name: 'Habitacion 1',
+      name: 'Habitacion 3',
       rate: 150,
       discount: 10,
     };
@@ -52,24 +52,130 @@ describe('Testing class Room', () => {
     expect(typeof isOccupiedTest1).toBe('boolean')
     expect(typeof isOccupiedTest2).toBe('boolean')
   })
-  it('occupancyPercentage nos retornar el porcentaje de dias ocupado, segun el rango de fechas que pasemos', () => {
+  it("occupancyPercentage tiene que devolver 100 si todas las fechas pasadas al metodo están dentro del rango de bookings", () => {
     const room = {
+      name: "Room1",
+      rate: 150,
+      discount: 10,
+    };
+
+    const booking1 = new Booking(
+      "booking 1",
+      "bok@bok.es",
+      "2023-09-01",
+      "2023-09-15",
+      10,
+      room
+    );
+
+    const booking2 = new Booking(
+      "booking 2",
+      "bok2@bok.es",
+      "2023-09-16",
+      "2023-09-30",
+      10,
+      room
+    );
+
+    const bookings = [booking1, booking2];
+
+    const room1 = new Room(room.name, bookings, room.rate, room.discount);
+    const percentage = room1.occupancyPercentage("2023-09-01", "2023-09-30");
+
+    expect(percentage).toBe(100);
+  });
+  it("occupancyPercentage tiene que devolver 100 si todas las fechas pasadas al metodo están dentro del rango de bookings", () => {
+    const room = {
+      name: "Room1",
+      rate: 150,
+      discount: 10,
+    };
+
+    const booking1 = new Booking(
+      "booking 1",
+      "bok@bok.es",
+      "2023-08-14",
+      "2023-08-16",
+      10,
+      room
+    );
+
+    const booking2 = new Booking(
+      "booking 2",
+      "bok2@bok.es",
+      "2023-08-18",
+      "2023-09-20",
+      10,
+      room
+    );
+
+    const bookings = [booking1, booking2];
+
+    const room1 = new Room(room.name, bookings, room.rate, room.discount);
+    const percentage = room1.occupancyPercentage("2023-08-16", "2023-08-18");
+
+    expect(percentage).toBe(66.7);
+  });
+  it('totalOccupancyPercentage nos devuelve el porcentaje promedio de ocupacion de todas las habitaiones', () => {
+    const roomA = {
       name: 'Habitacion 1',
       rate: 150,
       discount: 10,
     };
-    const booking1 = new Booking('booking 1', 'booking1@bo.com', '2023-10-01', '2023-10-06', 10, room);
-    const booking2 = new Booking('booking 2', 'booking1@bo.com', '2023-10-07', '2023-10-15', 10, room);
-    const bookings = [
+    const booking1 = new Booking('booking 1', 'booking1@bo.com', '2023-10-01', '2023-10-10', 10, roomA);
+    const booking2 = new Booking('booking 2', 'booking1@bo.com', '2023-10-11', '2023-10-15', 10, roomA);
+
+    const bookingsA = [
       booking1,
       booking2,
     ]
-    const room3 = new Room(room.name, bookings, room.rate, room.discount);
-    const percentageNumber = room3.occupancyPercentage('2023-10-01', '2023-10-15')
-    const percentageNumber2 = room3.occupancyPercentage('2023-10-01', '2023-11-10')
-    const percentageNumber3 = room3.occupancyPercentage('2023-09-26', '2023-09-01')
-    expect(percentageNumber).toBe(100.0)
-    expect(percentageNumber2).toBe(36.6)
-    expect(percentageNumber3).toBe(0)
+    const roomB = {
+      name: 'Habitacion 2',
+      rate: 150,
+      discount: 10,
+    };
+    const booking3 = new Booking('booking 3', 'booking3@bo.com', '2023-10-16', '2023-10-22', 10, roomB);
+    const booking4 = new Booking('booking 4', 'booking4@bo.com', '2023-10-23', '2023-10-31', 10, roomB);
+    
+    const bookingsB = [
+      booking3,
+      booking4,
+    ]
+    const room_1 = new Room(roomA.name, bookingsA, roomA.rate, roomA.discount);
+    const room_2 = new Room(roomB.name, bookingsB, roomB.rate, roomB.discount);
+    const arrayOfRooms = [room_1, room_2]
+    const totalOccupPer = Room.totalOccupancyPercentage(arrayOfRooms, '2023-10-01', '2023-10-30');
+    expect(totalOccupPer).toBe(50)
+  })
+  it('totalOccupancyPercentage nos devuelve el porcentaje promedio de ocupacion de todas las habitaiones', () => {
+    const roomA = {
+      name: 'Habitacion 1',
+      rate: 150,
+      discount: 10,
+    };
+    const booking1 = new Booking('booking 1', 'booking1@bo.com', '2023-05-01', '2023-05-06', 10, roomA);
+    const booking2 = new Booking('booking 2', 'booking1@bo.com', '2023-05-07', '2023-06-10', 10, roomA);
+
+    const bookingsA = [
+      booking1,
+      booking2,
+    ]
+    const roomB = {
+      name: 'Habitacion 2',
+      rate: 150,
+      discount: 10,
+    };
+    const booking3 = new Booking('booking 3', 'booking3@bo.com', '2023-05-11', '2023-05-14', 10, roomB);
+    const booking4 = new Booking('booking 4', 'booking4@bo.com', '2023-05-15', '2023-05-17', 10, roomB);
+    
+    const bookingsB = [
+      booking3,
+      booking4,
+    ]
+    const room_1 = new Room(roomA.name, bookingsA, roomA.rate, roomA.discount);
+    const room_2 = new Room(roomB.name, bookingsB, roomB.rate, roomB.discount);
+    const arrayOfRooms = [room_1, room_2]
+    const totalOccupPer = Room.totalOccupancyPercentage(arrayOfRooms, '2023-05-01', '2023-05-30');
+    expect(totalOccupPer).toBe(61.6)
   })
 })
